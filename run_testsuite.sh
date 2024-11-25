4a0e8e8b3aff07d6109a741073b71617e17d550f
Sebastien Loriot <sebastien.loriot@cgal.org>
2024-11-25 10:33:14 +0100

    clean up after testsuite

--- run_testsuite.sh
+++ run_testsuite.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /Users/sloriot/CGAL/testsuite
 PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin && /bin/bash ./autotest_cgal_with_ctest -s > /Users/sloriot/CGAL/testsuite/testsuite_run.log 2>&1
+
+tgz=`cat RELEASE_NR`
+echo rm  $tgz
+res_dir=`echo $tgz | sed 's/.tar.gz//'`
+echo rm -r $res_dir
+
